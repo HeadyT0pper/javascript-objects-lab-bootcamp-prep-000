787869d7a3550feb85837652db8fc44df614a5f4
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-02 20:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function deleteFromObjectByKey(object, key) {
   return recipes
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
